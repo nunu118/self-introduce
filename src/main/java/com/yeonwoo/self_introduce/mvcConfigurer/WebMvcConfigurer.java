ebf5fe342bce1e17deb7ed8dc48a95fdf50fc06e
jinseungbok <92.bokjs@gmail.com>
2026-07-08 19:14:35 +0900

    260708_spring 환경설정중

---
+++ src/main/java/com/yeonwoo/self_introduce/mvcConfigurer/WebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package com.yeonwoo.self_introduce.mvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+
+public class webConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.
+                addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/templates/")
+                .setCachePeriod(0);
+    }
+
+}
---
+++ src/main/java/com/yeonwoo/self_introduce/mvcConfigurer/webConfig.java
@@ -0,0 +1,9 @@
+package com.yeonwoo.self_introduce.mvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+
+public class webConfig {
+}
