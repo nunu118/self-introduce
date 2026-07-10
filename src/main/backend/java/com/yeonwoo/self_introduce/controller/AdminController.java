7da29320e3a43569dab391acecf92ed80379dd03
jinseungbok <92.bokjs@gmail.com>
2026-07-10 23:54:42 +0900

    250711_admin controller,service

--- src/main/backend/java/com/yeonwoo/self_introduce/controller/AdminController.java
+++ src/main/backend/java/com/yeonwoo/self_introduce/controller/AdminController.java
@@ -1,8 +1,19 @@
 package com.yeonwoo.self_introduce.controller;
 
+import com.yeonwoo.self_introduce.service.AdminService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class AdminController {
+    private final AdminService adminService;
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+    public void admin() {
+        adminService.admin();
+    }
+
+
 }
