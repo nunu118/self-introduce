b86ca8fbf4991a93d6ce100ab30192bb4a9e102a
jinseungbok <92.bokjs@gmail.com>
2026-07-24 18:33:04 +0900

    250724_CorsConfig, WebConfig

--- src/backend/java/com/yeonwoo/self_introduce/config/CorsConfig.java
+++ src/backend/java/com/yeonwoo/self_introduce/config/CorsConfig.java
@@ -6,15 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     // 허용할 Origin(application.yml)
     @Value("${app.cors.allowed-origin}")
     private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin);
+    }
 
     // CORS 정책 설정
     @Bean
---
+++ src/backend/java/com/yeonwoo/self_introduce/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.yeonwoo.self_introduce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${app.cours.allowed-origin")
+    private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
