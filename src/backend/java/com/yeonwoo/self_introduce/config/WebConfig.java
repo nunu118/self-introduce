1e694a051833ae1e6e6bd13472281ac88da3eb3d
jinseungbok <92.bokjs@gmail.com>
2026-07-24 18:37:11 +0900

    250724_CorsConfig, WebConfig

--- src/backend/java/com/yeonwoo/self_introduce/config/WebConfig.java
+++ src/backend/java/com/yeonwoo/self_introduce/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${app.cours.allowed-origin")
+    @Value("${app.cors.allowed-origin")
     private String allowedOrigin;
 
     @Override
